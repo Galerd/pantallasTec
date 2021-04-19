486bedd1a8b2354c0bfa2d4bb3d62749a0af6312
David Galieli Garcia Hinostroza <63182559+Galerd@users.noreply.github.com>
2021-04-19 01:45:31 -0600

    Scaffold Publicaciones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_042226) do
+ActiveRecord::Schema.define(version: 2021_04_19_072512) do
+
+  create_table "publications", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
