d9080558ef4706e739a2f547379e1ed0359c79bc
David Galieli Garcia Hinostroza <63182559+Galerd@users.noreply.github.com>
2021-04-20 00:29:20 -0600

    Diseño básico Administracion

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: publications
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  name        :string
+#  picture     :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Publication < ApplicationRecord  
     has_one_attached :picture
 end
--- test/models/publication_test.rb
+++ test/models/publication_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: publications
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  name        :string
+#  picture     :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require "test_helper"
 
 class PublicationTest < ActiveSupport::TestCase
