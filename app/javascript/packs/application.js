385dd12741983bd2b3dc160a98755ee11b4e4317
David Galieli Garcia Hinostroza <63182559+Galerd@users.noreply.github.com>
2021-04-22 18:22:57 -0600

    Navegacion Carrusel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,32 +14,3 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-function carrusel() {
-    window.addEventListener('load', function(){
-        console.log('cargado');
-
-        var img = [];
-        img [0] = '/images/1.jpg';
-        img [1] = '/images/2.jpg';
-        img [2] = '/images/3.jpg';
-        img [3] = '/images/4.jpg';
-        img [4] = '/images/5.jpg';
-
-        var i = 0;
-        var time = 2000;
-
-        function slide(){
-            
-            document.slider.src = img [i];
-            if(i < 4){
-                i++;
-            }else{
-                i = 0;
-            }
-        }
-
-        setInterval(slide, time);
-
-    });
-}
